a00ca8a94d8c9ec4e0c77210ef5ee870697cf16a
Lucas Carvalho <lucascarvalhoo@outlook.com>
2025-12-27 18:51:47 -0300

    Organizei a exibição da data

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ function inserir_tarefa(){
 event.preventDefault();
 const conteudo = document.getElementById('tarefa').value
 const prioridade = document.getElementById('prioridade').value
-const data = document.getElementById('inserir_data').value
-
+const data_dados = document.getElementById('inserir_data').value
+data =new Date(data_dados)
+let dia = ""
+let hora = ""
+if(!isNaN(data.getTime())){
+dia = "Dia: " + data.toLocaleDateString();
+hora = "as " + data.toLocaleTimeString();
+}
 const lista = document.createElement("div")
 lista.classList.add("bloco-tarefa")
 lista.innerHTML = `
     <button onclick="apagar(this)" class="apagar"><i class="fa-solid fa-delete-left"></i></button>
     <div class="conteudo">${conteudo}</div>
     <div class="prioridade-direito">Prioridade: ${prioridade}</div>
-    <div class="data">${data}</div>
+    <div class="data">${dia} ${hora}</div>
     `
 lista.dataset.prioridade = prioridade
 const container = document.getElementById('tarefa_direito')
