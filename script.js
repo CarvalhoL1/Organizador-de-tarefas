9b84f0a4774f380ef06e067c8be621a5b8504e6c
Lucas Carvalho <lucascarvalhoo@outlook.com>
2025-12-27 17:51:42 -0300

    Melhoria no js, barra de navegação

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ const data = document.getElementById('inserir_data').value
 const lista = document.createElement("div")
 lista.classList.add("bloco-tarefa")
 lista.innerHTML = `
-    <div class="prioridade-direito">Prioridade: ${prioridade}</div>
+    <button onclick="apagar(this)" class="apagar"><i class="fa-solid fa-delete-left"></i></button>
     <div class="conteudo">${conteudo}</div>
+    <div class="prioridade-direito">Prioridade: ${prioridade}</div>
     <div class="data">${data}</div>
     `
 document.getElementById('tarefa_direito').appendChild(lista)
+}
+function apagar(bloco){
+    bloco.parentElement.remove("bloco-tarefa")
 }
