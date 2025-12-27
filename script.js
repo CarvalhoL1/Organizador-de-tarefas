c76a21e05265244b537a50f88d1624cc2c453fc7
Lucas Carvalho <lucascarvalhoo@outlook.com>
2025-12-27 18:16:28 -0300

    Agora o js os organiza por prioridade

--- script.js
+++ script.js
@@ -12,8 +12,16 @@ lista.innerHTML = `
     <div class="prioridade-direito">Prioridade: ${prioridade}</div>
     <div class="data">${data}</div>
     `
+lista.dataset.prioridade = prioridade
+const container = document.getElementById('tarefa_direito')
 document.getElementById('tarefa_direito').appendChild(lista)
+const blocos = Array.from(container.children)
+blocos.sort(ordenar)
+blocos.forEach(bloco => container.appendChild(bloco))
 }
 function apagar(bloco){
-    bloco.parentElement.remove("bloco-tarefa")
+    bloco.parentElement.remove()
+}
+function ordenar(a, b){
+    return Number(b.dataset.prioridade) -  Number(a.dataset.prioridade)
 }
